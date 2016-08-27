74a57cbb6d9662f647db223e21db8cfa111ac73c
walid55 <iraqww26@gmail.com>
2016-08-28 00:59:26 +0300

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,9 +224,13 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "add",
+    "lock_media",
+    "ar-redis",
+    "ar-run1",
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {188248946,262337203,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
